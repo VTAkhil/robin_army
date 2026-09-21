c8eb5185b89947af9e76558d23569fa2f2cc6420
Akhil Haldar <akhil@vertitect.com>
2026-09-21 18:53:22 +0530

    Change file path

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3307095362"
+    serviceWorkerVersion: "4112847379"
   }
 });
